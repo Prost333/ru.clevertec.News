634cb67a5b9d000aa3b24560a6034686421ff2a2
prost333 <dimabolshoy333@yandex.by>
2024-03-05 19:57:47 +0300

    test

--- src/main/java/ru/clevertec/ManagementNews/multiFeign/user/UserClient.java
+++ src/main/java/ru/clevertec/ManagementNews/multiFeign/user/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient(name = "user-service", url = "localhost:8081")
+@FeignClient(name = "user-service", url = "http://app:8081")
 public interface UserClient {
     @GetMapping("/users/{userId}")
     User getUser(@PathVariable Long userId);
